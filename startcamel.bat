3351974aad9addd87b720c7183eb512d6100f15d
David Sharpe <david.a.sharpe@cgi.com>
2020-07-23 09:45:28 -0700

    Minor refactor of batch script. Needs three windows only, not four.

--- startcamel.bat
+++ startcamel.bat
@@ -1,6 +1,4 @@
 echo off
 start "hnclient-v2" /d hnclient-v2 cmd /k "mvn compile camel:run"
 start "mock-hnsecure" /d mock-hnsecure cmd /k "mvn compile camel:run"
-echo Wait for the Camel programs to finish starting, then press any key to send a message
-pause
-start "mock-point-of-service" /d mock-point-of-service cmd /k "mvn compile exec:java"
+start "mock-point-of-service" /d mock-point-of-service cmd /k "echo Wait for Camel programs to start, then && pause && mvn compile exec:java"
